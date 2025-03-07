2d18f63cb81a9b7c5ae4c430f1261392c2602348
Mike Hacker <61195478+mhackermsft@users.noreply.github.com>
2025-03-07 08:52:32 -0500

    Added escaping for HTML code blocks to  ensure proper formatting of displayed code.

--- BlazorAIChat/wwwroot/app.js
+++ BlazorAIChat/wwwroot/app.js
@@ -8,9 +8,24 @@ customElements.define('assistant-message', class extends HTMLElement {
 
     attributeChangedCallback(name, oldValue, newValue) {
         if (name === 'markdown') {
+
+            // Remove <citation> tags
             newValue = newValue.replace(/<citation.*?<\/citation>/gs, '');
-            const elements = marked.parse(newValue.replace(/</g, '&lt;'));
-            this.innerHTML = purify.sanitize(elements, { KEEP_CONTENT: false });
+
+            // Parse the markdown to HTML
+            const elements = marked.parse(newValue);
+
+            // Sanitize the HTML
+            const sanitizedElements = purify.sanitize(elements, { KEEP_CONTENT: false });
+
+            // Escape HTML code blocks
+            const escapedHtml = sanitizedElements.replace(/<code>(.*?)<\/code>/gs, (match, p1) => {
+                return `<code>${p1.replace(/</g, '&lt;').replace(/>/g, '&gt;')}</code>`;
+            });
+
+            // Set the innerHTML
+            this.innerHTML = escapedHtml;
+
         }
     }
 });
